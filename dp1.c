8403d59eed257af93030a6bab35abf2d3ea00332
CK <chingun@umich.edu>
2022-02-20 18:42:35 -0500

    whatever

--- dp1.c
+++ dp1.c
@@ -1,43 +1,42 @@
 #include <stdio.h>
+#include <math.h>
 #include <time.h>
-
+#include <stdlib.h>
 
 float dp(long N, float *pA, float *pB) {
     float R = 0.0;
-    int j;
-    for (j = 0; j < N; j++)
-        R += pA[j] * pB[j];
-    return R;
+	int j;
+	for (j=0;j<N;j++) 
+		R += pA[j]*pB[j]; 
+	return R;
 }
 
-
 int main(int argc, char *argv[]) {
-    // retrieve the sizes of the vectors to calculate
-    int vector_size = 1000;
-    int compute_amount = 10;
+	struct timespec start, end;
+	float* pA;
+	float* pB;
+	long N;
+	int R;
+	char* p;
     
-    if (argc != 2){
-        printf("The input is invalid");
-        return;
+    if(argc < 3){
+    	printf("Error: R and N values not given. File should run in format ./dp1 1000 10 \n");
+    	return 0;
     } else {
-        vector_size = argv[1];
-        compute_amount = argv[2];
-        printf("Vectors to compute by is: \narg%d=%s", 1, vector_size);
-        printf("Amount of times to recompute: \narg%d=%s", 2, compute_amount);
-    }
-    
-    float* first = (float*) malloc(vector_size * sizeof(float));
-    float* second = (float*) malloc(vector_size * sizeof(float));
-    
-    // Set all the variables in the vector
-    for (int i = 0; i < vector_size; i++) {
-        *(first + i) = 1.0;
-        *(second + i) = 1.0;
-    }
-    
-    for (int i = 0; i < compute_amount; i++){
-        dp(vector_size, first, second);
-    }
-    
-    return 0;
+        N = (long)strtol(argv[1], &p, 10);
+        R = (int)strtol(argv[2], &p, 10);
+        pA = (float*)malloc(N * sizeof(float));
+        pB = (float*)malloc(N * sizeof(float));
+        for (int i = 0; i < N; ++i) { 
+            *(pA + i) = 1.0;
+            *(pB + i) = 1.0;
+	} 
+
+	clock_gettime(CLOCK_MONOTONIC, &start); 			
+	for (int i = 0; i < R; ++i)
+		dp(N, pA, pB);
+	clock_gettime(CLOCK_MONOTONIC, &end);
+	
+	double time_usec=(((double)end.tv_sec *1000000 + (double)end.tv_nsec/1000) - ((double)start.tv_sec * 1000000 + (double)start.tv_nsec/1000));
+	printf("N: %ld T: %.03lf B: %.03f F: %.03f", N, time_usec, (2 * N * sizeof(float)/time_usec/10e9) ,(2*N*(R/2))/time_usec);
 }
