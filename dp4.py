27719ebca7ae5b312b292e56cea676ecd7d4b28b
CK <chingun@umich.edu>
2022-02-21 00:41:28 -0500

    dasd

--- dp4.py
+++ dp4.py
@@ -17,26 +17,26 @@ def dp(N, A, B):
         R += A[j]*B[j]
     return R
 
-for i in range(0, (int)(R/2)):
-    dp(N, A, B)
+# for i in range(0, (int)(R/2)):
+#     dp(N, A, B)
 
-t_start=time.monotonic()
-for i in range(0, (int)(R/2)):
-    dp(N, A, B)
-t_end=time.monotonic()
+# t_start=time.monotonic()
+# for i in range(0, (int)(R/2)):
+#     dp(N, A, B)
+# t_end=time.monotonic()
 
-t = (t_end - t_start) * 1.0 / (R / 2.0)
-flops = (2 * N * (R / 2.0)) / (t * 1000000000.0)
-bandwidth = (2 * N * sys.getsizeof(float) * (R / 2.0)) / (t * 1000000000.0)
-print("N: ", N, " T: ", t , " sec B: " , bandwidth, " GB/sec F: ", flops, " GFLOPS/sec")
-print("R: ", R)
+# t = (t_end - t_start) * 1.0 / (R / 2.0)
+# flops = (2 * N * (R / 2.0)) / (t * 1000000000.0)
+# bandwidth = (2 * N * sys.getsizeof(float) * (R / 2.0)) / (t * 1000000000.0)
+# print("N: ", N, " T: ", t , " sec B: " , bandwidth, " GB/sec F: ", flops, " GFLOPS/sec")
+# print("R: ", R)
 
 N = 300000000
 R = 20
 A = np.ones(N,dtype=np.float32)
 B = np.ones(N,dtype=np.float32)
 
-for i in range(0, (int)(R/2)):
+for i in range(0, (int)(3)):
     dp(N, A, B)
 
 t_start=time.monotonic()
