e2091e787edec8584765b83e218eab11322cf2f8
CK <chingun@umich.edu>
2022-02-20 19:37:30 -0500

    hmm

--- dp1.c
+++ dp1.c
@@ -47,8 +47,8 @@ int main(int argc, char *argv[]) {
 	
 	time = (((double)end.tv_sec * 1000000 + (double)end.tv_nsec / 1000) - ((double)start.tv_sec * 1000000 + (double)start.tv_nsec / 1000));
     time = (time / (R / 2.0));
-    flops = (3 * N) / (time * 1000000000.0);
-    bandwidth = (8 * N) / (time * 1000000000.0);
+    flops = (2 * N) / (time * 1000000000.0);
+    bandwidth = (2 * N * sizeof(float)) / (time * 1000000000.0);
 
 	printf("N: %ld T: %.03lf sec B: %.03f GB/sec F: %.03f GFLOP/sec\n", N, time, bandwidth, flops);
 }
