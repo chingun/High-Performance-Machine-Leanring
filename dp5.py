331cdf3d85be0b78f2812f2401403fe166621eb7
CK <chingun@umich.edu>
2022-02-20 23:12:05 -0500

    tuff

--- dp5.py
+++ dp5.py
@@ -3,14 +3,14 @@ import numpy as np
 import sys
 
 
+
+N = 1000000
+R = 1000 
 A = np.ones(N,dtype=np.float32)
 B = np.ones(N,dtype=np.float32)
 t_start = 0
 t_end = 0
 t = 0
-
-N = 1000000
-R = 1000 
 
 for i in range(0, (int)(R/2)):
     np.dot(A, B)
@@ -27,6 +27,8 @@ print("N: ", N, " T: ", t , " sec B: " , bandwidth, " GB/sec F: ", flops, " GFLO
 
 N = 300000000
 R = 20 
+A = np.ones(N,dtype=np.float32)
+B = np.ones(N,dtype=np.float32)
 
 for i in range(0, (int)(R/2)):
     np.dot(A, B)
