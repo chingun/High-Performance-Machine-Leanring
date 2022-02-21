950637c564295c5fcb27ecc0a67b39a5b3742031
CK <chingun@umich.edu>
2022-02-20 23:10:00 -0500

    hello

--- dp4.py
+++ dp4.py
@@ -30,3 +30,19 @@ t = (t_end - t_start) * 1.0 / (R / 2.0)
 flops = (2 * N * (R / 2.0)) / (t * 1000000000.0)
 bandwidth = (2 * N * sys.getsizeof(float) * (R / 2.0)) / (t * 1000000000.0)
 print("N: ", N, " T: ", t , " sec B: " , bandwidth, " GB/sec F: ", flops, " GFLOPS/sec \n")
+
+N = 300000000
+R = 20
+
+for i in range(0, (int)(R/2)):
+    dp(N, A, B)
+
+t_start=time.monotonic()
+for i in range(0, (int)(R/2)):
+    dp(N, A, B)
+t_end=time.monotonic()
+
+t = (t_end - t_start) * 1.0 / (R / 2.0)
+flops = (2 * N * (R / 2.0)) / (t * 1000000000.0)
+bandwidth = (2 * N * sys.getsizeof(float) * (R / 2.0)) / (t * 1000000000.0)
+print("N: ", N, " T: ", t , " sec B: " , bandwidth, " GB/sec F: ", flops, " GFLOPS/sec \n")
